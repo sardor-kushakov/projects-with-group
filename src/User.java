4a5cb42cc01c3d7eab97c286c80e90b06962d588
Zaripov <ergashaliz7008@gmail.com>
2024-06-12 18:22:42 +0500

    add User class

---
+++ src/User.java
@@ -0,0 +1,55 @@
+public class User {
+    private String name;
+    private String surname;
+    private String address;
+    private int room;
+
+    public User(String name, String surname, String address, int room) {
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", address='" + address + '\'' +
+                ", room=" + room +
+                '}';
+    }
+}
